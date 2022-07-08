b988b6f10bd099873a43caca68ef41febcd723b2
priyank0306 <92321587+priyank0306@users.noreply.github.com>
2022-07-08 12:11:46 +0530

    Add files via upload

---
+++ Level_Order_Traversal.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        if(root==NULL)
+            return ans;
+        
+        while(!q.empty())
+        {
+            vector<int>level;
+            int n=q.size();
+            for(int i=0; i<n; i++)
+            {
+                    TreeNode* temp=q.front();
+                level.push_back(temp->val);
+                q.pop();
+                 if(temp->left!=NULL)
+            {
+                q.push(temp->left);
+}
+                        if(temp->right!=NULL)
+            {
+                q.push(temp->right);
+}
+                
+}
+    ans.push_back(level);        
+                
+     
+            
+}
+        return ans;
+    }
+};
